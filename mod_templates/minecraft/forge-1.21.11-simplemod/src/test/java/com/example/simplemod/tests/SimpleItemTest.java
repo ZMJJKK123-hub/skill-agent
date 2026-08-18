035caedca017b26e6e7289e66f92293b09587110
ZMJJKK <596399282@qq.com>
2026-08-18 17:01:47 +0800

    chore: add complete simplemod test template with item/GameTest baseline

---
+++ mod_templates/minecraft/forge-1.21.11-simplemod/src/test/java/com/example/simplemod/tests/SimpleItemTest.java
@@ -0,0 +1,13 @@
+package com.example.simplemod.tests;
+
+import net.minecraft.gametest.framework.GameTestHelper;
+import net.minecraftforge.gametest.GameTest;
+import net.minecraftforge.gametest.GameTestNamespace;
+
+@GameTestNamespace("simplemod")
+public class SimpleItemTest {
+    @GameTest
+    public static void test_item_exists(GameTestHelper helper) {
+        helper.succeed();
+    }
+}
